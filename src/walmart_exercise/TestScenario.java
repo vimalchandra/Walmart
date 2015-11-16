b055b176a0d75e1d85ea48de8dbd9028ac64ce6b
Vimal <vimalchandra.gorijala@sjsu.edu>
2015-11-15 21:13:05 -0800

    Added Comments

--- src/walmart_exercise/TestScenario.java
+++ src/walmart_exercise/TestScenario.java
@@ -1,30 +1,79 @@
 package walmart_exercise;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Properties;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 
 public class TestScenario extends Reusable_Functions {
-	public static void main(String args[]) throws InterruptedException
+	public static void main(String args[]) throws InterruptedException, FileNotFoundException, IOException
 	{
+		Properties objects=new Properties();
+		objects.load(new FileInputStream("ObjectRepository/objects.properties"));
 		String Quantity ="1";
-		String Item_Input ="Apple <mark>iPhone</mark> 5C 8GB Verizon (Locked)";
-		LaunchApplication();
-		Login("vimalchandra.gorijala@sjsu.edu","sanjose");
-		Search("iphone");
-		String productId=SelectItem(Item_Input);
-		System.out.println("Product Id:"+productId);
+		String Item_Input ="Hanes Men's 6 Pack  Crew";
+		
+		/**
+		 * @author Vimal
+		   Launches Chrome browser with given URL 
+		 */
+		
+		LaunchApplication("GoogleChrome","https://www.walmart.com/account/login?returnUrl=%2Faccount%2F");
+		
+		/**
+		 * @author Vimal
+		   Logs into the account with given credentials
+		 */
+		
+		Login(objects,"vimalchandra.gorijala@sjsu.edu","sanjose");
+		
+		/**
+		 * @author Vimal
+		   Searches the item given and displays the results 
+		 */
+		
+		SearchItem(objects,"socks");
+		
+		/**
+		 * @author Vimal
+		   Loads the item page of given input item and collects 'data-item-id' of it
+		 */
+		String productId=SelectItem(objects,Item_Input);
 		Thread.sleep(3000);
-		SelectItemQuantity(Quantity);
+		
+		/**
+		 * @author Vimal
+		   Selects the quantity of the given item
+		 */
+		
+		SelectItemQuantity(objects,Quantity);
 		Thread.sleep(3000);
-		AddItemstoCart();
-		ViewCartItems();
-		WebElement ViewCartItemInfo = driver.findElement(By.id("CartItemInfo"));
-		WebElement ViewCartItemQuantity= driver.findElement(By.xpath(".//*[@id='spa-layout']/div/div/div[1]/div/div[4]/div[2]/div/div/div[4]/div/div[1]/div/div/div/div[1]/div/div"));
-		System.out.println(ViewCartItemInfo.getAttribute("data-us-item-id"));
-		System.out.println(ViewCartItemQuantity.getAttribute("innerHTML"));
-		ValidateCartItem(ViewCartItemInfo,productId,ViewCartItemQuantity,Quantity);
-
+		
+		/**
+		 * @author Vimal
+		   Adds the item to the cart
+		 */
+		
+		AddItemstoCart(objects);
+		
+		/**
+		 * @author Vimal
+		   Views the items in the cart
+		 */
+		
+		ViewCartItems(objects);
+		
+		/**
+		 * @author Vimal
+		   Validates the item in the cart
+		 */
+		
+		WebElement viewcartiteminfo = driver.findElement(By.id(objects.getProperty("viewcartiteminfo")));
+		WebElement viewcartitemquantity = driver.findElement(By.xpath(objects.getProperty("viewcartitemquantity")));
+		ValidateCartItem(viewcartiteminfo,productId,viewcartitemquantity,Quantity);
+		driver.quit();
 	}
-
-
 }
