02afe5c8202ae9cfe216a68fd30774f758f751da
Vimal <vimalchandra.gorijala@sjsu.edu>
2015-11-15 21:15:00 -0800

    Minor Changes

--- src/walmart_exercise/Reusable_Functions.java
+++ src/walmart_exercise/Reusable_Functions.java
@@ -1,14 +1,17 @@
 package walmart_exercise;
 
 
-import java.util.Iterator;
 import java.util.List;
+import java.util.Properties;
 import java.util.concurrent.TimeUnit;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.remote.RemoteWebElement;
+import org.openqa.selenium.safari.SafariDriver;
 import org.openqa.selenium.support.ui.Select;
 
 
@@ -16,6 +19,7 @@ import org.openqa.selenium.support.ui.Select;
  * @author Vimal
    This class consists of all reusable functions 
  */
+
 public class Reusable_Functions {
 	
 	public static WebDriver driver;
@@ -29,86 +33,113 @@ public class Reusable_Functions {
 	 * @author Vimal
 	   Method for launching the requested Web page
 	 */
-	public static void LaunchApplication() throws InterruptedException
+	
+	public static void LaunchApplication(String browserName, String URL) throws InterruptedException
 	{
+		if(browserName.equalsIgnoreCase("googlechrome"))
+		{
 		System.setProperty("webdriver.chrome.driver", "C:\\Chromedriver.exe");
 		driver = new ChromeDriver();
-		waitimplicitly(2);
-		driver.navigate().to("https://www.walmart.com/account/login?returnUrl=%2Faccount%2F");
+		waitimplicitly(2);		
+		}
+		else if(browserName.equalsIgnoreCase("ie"))
+		{
+			System.setProperty("webdriver.ie.driver", "C:\\iexplore.exe");
+			driver = new ChromeDriver();
+			waitimplicitly(2);		
+		}
+		else if(browserName.equalsIgnoreCase("firefox"))
+		{
+			driver=new FirefoxDriver();
+			waitimplicitly(2);
+		}
+		else if(browserName.equalsIgnoreCase("safari"))
+		{
+			driver=new SafariDriver();	
+			waitimplicitly(2);
+		}
+		driver.navigate().to(URL);
 		driver.manage().window().maximize();
-		Thread.sleep(3000);
+		Thread.sleep(3000);			
+		
 	}
+	
 	/**
 	 * @author Vimal
 	   Method for logging into the account
 	 */
-	public static void Login(String username, String password) throws InterruptedException
+	
+	public static void Login(Properties objects,String username, String password) throws InterruptedException
 	{
-		WebElement login_textbox = driver.findElement(By.id("login-username"));
+		WebElement login_textbox = driver.findElement(By.id(objects.getProperty("username")));
 		login_textbox.sendKeys(username);
-		WebElement passwd_textbox = driver.findElement(By.id("login-password"));
+		WebElement passwd_textbox = driver.findElement(By.id(objects.getProperty("password")));
 		passwd_textbox.sendKeys(password);
-		WebElement signin_button = driver.findElement(By.xpath("html/body/div[2]/section/section[4]/div/div/div/div/div/div/div/form/div/button"));
+		WebElement signin_button = driver.findElement(By.xpath(objects.getProperty("signin_button")));
 		signin_button.click();
 		Thread.sleep(3000);
 	}
+	
 	/**
 	 * @author Vimal
 	   Method for searching requested item 
 	 */
-	public static void Search(String term) throws InterruptedException
+	
+	public static void SearchItem(Properties objects,String item) throws InterruptedException
 	{
-		WebElement search_textbox = driver.findElement(By.id("search"));
-		search_textbox.sendKeys(term);
+		WebElement search_textbox = driver.findElement(By.id(objects.getProperty("search_textbox")));
+		search_textbox.sendKeys(item);
 		Thread.sleep(4000);
-		WebElement search_button = driver.findElement(By.xpath(".//*[@id='top']/div[3]/div/div/div/div/div[3]/form/div/div[3]/button"));
+		WebElement search_button = driver.findElement(By.xpath(objects.getProperty("search_button")));
 		waitimplicitly(2);
 		search_button.click();
-		Thread.sleep(4000);
-		
+		Thread.sleep(4000);		
 	}
+	
 	/**
 	 * @author Vimal
 	   Method for searching requested item 
 	 */
-	public static String SelectItem(String title) throws InterruptedException
+	
+	public static String SelectItem(Properties objects,String ItemName) throws InterruptedException
 	{
-		WebElement listview = driver.findElement(By.xpath(".//*[@id='utility-bar-container']/div/div[2]/a[2]"));
+		WebElement listview = driver.findElement(By.xpath(objects.getProperty("listview")));
 		listview.click();
 		Thread.sleep(3000);
-		List<WebElement> list_values = driver.findElements(By.xpath(".//*[@id='tile-container']/div[*]"));
-		//System.out.println(drop.size()+" "+drop.isEmpty());
+		List<WebElement> list_values = driver.findElements(By.xpath(objects.getProperty("list_values")));
 		String value=null;
 		for(WebElement item:list_values)
 		{	
-			//System.out.println(item.getAttribute("innerHTML"));
-			if(item.findElement(By.xpath("./div/div/h4/a")).getAttribute("innerHTML").contains(title))
+			if(item.findElement(By.xpath(objects.getProperty("list_item"))).getAttribute("innerHTML").contains(ItemName))
 			{
 				value=item.getAttribute("data-item-id");
-				item.findElement(By.xpath("./div/div/h4/a")).click();
+				item.findElement(By.xpath(objects.getProperty("list_item"))).click();
 				return value;
 			}	
 		}
 		return value;
 	}
+	
 	/**
 	 * @author Vimal
 	   Method for selecting requested number of items 
 	 */
-	public static void SelectItemQuantity(String visibletext)
+	
+	public static void SelectItemQuantity(Properties objects,String visibletext)
 	{
-		 WebElement quantity_dropdown = driver.findElement(By.id("WMItemQtyDropDown"));
+		 WebElement quantity_dropdown = driver.findElement(By.id(objects.getProperty("quantity_dropdown")));
 		 Select quant = new Select(quantity_dropdown);
-		 quant.selectByVisibleText(visibletext);
-		 
+		 quant.selectByVisibleText(visibletext);		 
 	}
+	
 	/**
 	 * @author Vimal
 	   Method for adding items to the cart 
 	 */
-	public static void AddItemstoCart() throws InterruptedException
+	
+	public static void AddItemstoCart(Properties objects) throws InterruptedException
 	{
-		WebElement addcart_button = driver.findElement(By.id("WMItemAddToCartBtn"));
+		WebElement addcart_button = driver.findElement(By.id(objects.getProperty("addcart_button")));
 		
 		if(addcart_button.isDisplayed()==true)
 		{
@@ -120,20 +151,24 @@ public class Reusable_Functions {
 		}
 		Thread.sleep(3000);
 	}
+	
 	/**
 	 * @author Vimal
 	   Method for viewing items in the cart 
 	 */
-	public static void ViewCartItems() throws InterruptedException
+	
+	public static void ViewCartItems(Properties objects) throws InterruptedException
 	{
-		WebElement viewcart_button = driver.findElement(By.id("PACViewCartBtn"));
+		WebElement viewcart_button = driver.findElement(By.id(objects.getProperty("viewcart_button")));
 		viewcart_button.click();
 		Thread.sleep(2000);
 	}
+	
 	/**
 	 * @author Vimal
 	   Method for validating the items in the cart 
 	 */
+	
 	public static void ValidateCartItem(WebElement z,String value,WebElement z1,String itemsincart)
 	{
 		String valuepresent = String.valueOf(z1.getAttribute("innerHTML"));
