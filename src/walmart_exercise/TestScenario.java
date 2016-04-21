044289b3dc92dd5e97183e817aecfdc3b1e88275
vimalchandra <vimalchandra.gorijala@sjsu.edu>
2016-04-20 20:48:40 -0700

    Update TestScenario.java

--- src/walmart_exercise/TestScenario.java
+++ src/walmart_exercise/TestScenario.java
@@ -16,58 +16,50 @@ public class TestScenario extends Reusable_Functions {
 		String Quantity ="1";
 		String Item_Input ="Hanes Men's 6 Pack  Crew";
 		
-		/**
-		 * @author Vimal
+		/*
 		   Launches Chrome browser with given URL 
 		 */
 		
 		LaunchApplication("GoogleChrome","https://www.walmart.com/account/login?returnUrl=%2Faccount%2F");
 		
-		/**
-		 * @author Vimal
+		/*
 		   Logs into the account with given credentials
 		 */
 		
 		Login(objects,"vimalchandra.gorijala@sjsu.edu","sanjose");
 		
-		/**
-		 * @author Vimal
+		/*
 		   Searches the item given and displays the results 
 		 */
 		
 		SearchItem(objects,"socks");
 		
-		/**
-		 * @author Vimal
+		/*
 		   Loads the item page of given input item and collects 'data-item-id' of it
 		 */
 		String productId=SelectItem(objects,Item_Input);
 		Thread.sleep(3000);
 		
-		/**
-		 * @author Vimal
+		/*
 		   Selects the quantity of the given item
 		 */
 		
 		SelectItemQuantity(objects,Quantity);
 		Thread.sleep(3000);
 		
-		/**
-		 * @author Vimal
+		/*
 		   Adds the item to the cart
 		 */
 		
 		AddItemstoCart(objects);
 		
-		/**
-		 * @author Vimal
+		/*
 		   Views the items in the cart
 		 */
 		
 		ViewCartItems(objects);
 		
-		/**
-		 * @author Vimal
+		/*
 		   Validates the item in the cart
 		 */
 		
